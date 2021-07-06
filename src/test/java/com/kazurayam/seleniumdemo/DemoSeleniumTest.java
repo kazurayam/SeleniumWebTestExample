846c88eb0de633c557e518cb03885004d28ebd12
kazurayam <kazuaki.urayama@gmail.com>
2021-07-06 09:25:03 +0900

    updates

--- src/test/java/com/kazurayam/seleniumdemo/DemoSeleniumTest.java
+++ src/test/java/com/kazurayam/seleniumdemo/DemoSeleniumTest.java
@@ -3,10 +3,10 @@ package com.kazurayam.seleniumdemo;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -17,22 +17,30 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Stream;
 
-//@Disabled
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assumptions.assumeFalse;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
+import static org.junit.jupiter.api.Assumptions.assumingThat;
+
+
+/**
+ * An example of Selenium WebDriver testing based on https://www.linkedin.com/pulse/running-selenium-web-tests-github-actions-moataz-nabil
+ * An example of JUnit5 Test class based on https://www.baeldung.com/junit-5
+ */
 public class DemoSeleniumTest {
 
     private static Logger logger = LoggerFactory.getLogger(DemoSeleniumTest.class);
 
-    private WebDriver driver;
+    private static WebDriver driver;
 
     @BeforeAll
     public static void beforeAll() {
-        logger.info("@BeforeAll was called");
-    }
-
-    @BeforeEach
-    public void setUp()
-    {
+        logger.debug("@BeforeAll was called");
         WebDriverManager.chromedriver().setup();
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--no-sandbox");
@@ -44,9 +52,17 @@ public class DemoSeleniumTest {
         driver.manage().timeouts().implicitlyWait(120, TimeUnit.MILLISECONDS);
     }
 
+    @BeforeEach
+    public void setUp()
+    {
+        logger.debug("@BeforeEach was called");
+    }
+
+    @DisplayName("A user logs into it")
     @Test
     public void userLogin()
     {
+        logger.info("userLogin() started");
         assert driver != null;
         WebElement usernameTxt = driver.findElement(By.id("username"));
         usernameTxt.sendKeys("tomsmith");
@@ -55,19 +71,79 @@ public class DemoSeleniumTest {
         WebElement submitBtn = driver.findElement(By.className("radius"));
         submitBtn.click();
         logger.info("Current URL is:" + driver.getCurrentUrl());
-        Assertions.assertTrue(driver.getCurrentUrl().contains("secure"));
+        assertTrue(driver.getCurrentUrl().contains("secure"));
+    }
+
+    @Test
+    @Disabled("Not implemented yet")
+    void testShowSomething() {
+    }
+
+    @Test
+    void lambdaExpressions() {
+        assertTrue(Stream.of(1, 2, 3)
+                .mapToInt(i -> i)
+                .sum() > 5, () -> "Sum should be greater than 5");
+    }
+
+    @Test
+    void groupAssertions() {
+        int[] numbers = {0, 1, 2, 3, 4};
+        assertAll("numbers",
+                () -> assertEquals(numbers[0], 0),
+                () -> assertEquals(numbers[3], 3),
+                () -> assertEquals(numbers[4], 4)
+        );
+    }
+
+    @Test
+    void trueAssumption() {
+        assumeTrue(5 > 1);
+        assertEquals(5 + 2, 7);
+    }
+
+    @Test
+    void falseAssumption() {
+        assumeFalse(5 < 1);
+        assertEquals(5 + 2, 7);
+    }
+
+    @Test
+    void assumptionThat() {
+        String someString = "Just a string";
+        assumingThat(
+                someString.equals("Just a string"),
+                () -> assertEquals(2 + 2, 4)
+        );
+    }
+
+    @Test
+    void shouldThrowException() {
+        Throwable exception = assertThrows(UnsupportedOperationException.class, () -> {
+            throw new UnsupportedOperationException("Not supported");
+        });
+        assertEquals(exception.getMessage(), "Not supported");
+    }
+
+    @Test
+    void assertThrowsException() {
+        String str = null;
+        assertThrows(IllegalArgumentException.class, () -> {
+            Integer.valueOf(str);
+        });
     }
 
     @AfterEach
     public void tearDown(){
-        if (driver != null) {
-            driver.quit();
-        }
+        logger.debug("@AfterEach was called");
     }
 
     @AfterAll
     public static void afterAll() {
-        logger.info("@AfterAll was called");
+        logger.debug("@AfterAll was called");
+        if (driver != null) {
+            driver.quit();
+        }
     }
 
 }
---
+++ src/test/java/com/kazurayam/seleniumdemo/TestSuiteBySelectClasses.java
@@ -0,0 +1,9 @@
+package com.kazurayam.seleniumdemo;
+
+import org.junit.platform.suite.api.SelectClasses;
+
+@SelectClasses({DemoSeleniumTest.class, DemoTakingScreenshotsDiffTest.class})
+public class TestSuiteBySelectClasses
+{
+
+}
